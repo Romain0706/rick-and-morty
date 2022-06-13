db250dd8350614960bb7a0df6afcd8c2bb747a9c
Romain Boniface <romain.boniface@thetribe.io>
2022-06-13 20:12:54 +0200

    feat(api): install vue-query

--- src/views/Character/CharactersView.vue
+++ src/views/Character/CharactersView.vue
@@ -1,3 +1,19 @@
 <template>
   <h1>Characters</h1>
+  <div v-for="character in data?.results" :key="character.id">
+    <img :src="character.image" />
+  </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useQuery } from 'vue-query';
+
+export default defineComponent({
+  setup() {
+    const { isLoading, isError, data, error, isFetching } = useQuery('characters', async () => await fetch('https://rickandmortyapi.com/api/character').then((res) => res.json()));
+
+    return { isLoading, isError, data, error, isFetching };
+  },
+})
+</script>
